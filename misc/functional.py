7c312987ac7dbcf6ff612c229dbb31993137fe8f
Takahiro Itazuri <takahiro.lab.1226@gmail.com>
2019-03-09 22:41:50 +0900

    add sampling from truncated normal distribution

--- misc/functional.py
+++ misc/functional.py
@@ -6,7 +6,8 @@ import torch
 
 __all__ = [
 	'label2onehot',
-	'calc_norm_each_sample'
+	'calc_norm_each_sample',
+	'truncated_normal'
 ]
 
 
@@ -29,3 +30,12 @@ def calc_norm_each_sample(x, p=2):
 	"""
 	return x.view(x.size(0), -1).norm(p=p, dim=1)[:, None, None, None]
 
+
+def truncated_normal(shape, mean=0.0, std=1.0):
+	x = torch.randn(shape)
+	tmp = x.new_empty(shape + (4,)).normal_()
+	valid = (tmp < 2) & (tmp > -2)
+	idx = valid.max(-1, keepdim=True)[1]
+	x.data.copy_(tmp.gather(-1, idx).squeeze(-1))
+	x.data.mul_(std).add_(mean)
+	return x
