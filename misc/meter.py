668ffd36a7e0fd072ac58689535c4e312daf7c73
Takahiro Itazuri <takahiro.lab.1226@gmail.com>
2019-03-10 09:43:26 +0900

    add AverageMeter

---
+++ misc/meter.py
@@ -0,0 +1,20 @@
+__all__ = [
+	'AverageMeter'
+]
+
+
+class AverageMeter(object):
+	def __init__(self):
+		self.reset()
+
+	def reset(self):
+		self.val = 0
+		self.avg = 0
+		self.sum = 0
+		self.cnt = 0
+
+	def update(self, val, n=1):
+		self.val = val
+		self.sum += val * n
+		self.cnt += n
+		self.avg = self.sum / self.cnt
