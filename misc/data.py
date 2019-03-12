8f62657c8f6dd7e2b6b6ad3a5b16574d78d6364e
Takahiro Itazuri <takahiro.lab.1226@gmail.com>
2019-03-12 14:53:37 +0900

    add datasets

--- misc/data.py
+++ misc/data.py
@@ -1,5 +1,6 @@
 import os
 import sys
+import csv
 import random
 import urllib
 import zipfile
@@ -38,34 +39,36 @@ __all__ = [
 # https://github.com/Armour/pytorch-nn-practice/blob/master/utils/meanstd.py
 # https://discuss.pytorch.org/t/computing-the-mean-and-std-of-dataset/34949
 means = {
-	'mnist'     : [0.13066049],
-	'svhn'      : [0.43768210, 0.44376970, 0.47280442],
-	'cifar10'   : [0.49139968, 0.48215841, 0.44653091],
-	'cifar100'  : [0.50707516, 0.48654887, 0.44091784],
-	'stl10'     : [0.44671062, 0.43980984, 0.40664645],
-	'lsun'      : [0.485, 0.456, 0.406], # copied from imagenet
-	'cub200'    : [0.47199252, 0.47448921, 0.41031790],
-	'dog120'    : [0.46614176, 0.42734158, 0.37495670],
-	'food101'   : [0.56214333, 0.42337474, 0.28560150],
-	'flower102' : [0.64367676, 0.46372274, 0.40175039],
-	'imagenet'  : [0.485, 0.456, 0.406]
+	'mnist'         : [0.13066049],
+	'svhn'          : [0.43768210, 0.44376970, 0.47280442],
+	'cifar10'       : [0.49139968, 0.48215841, 0.44653091],
+	'cifar100'      : [0.50707516, 0.48654887, 0.44091784],
+	'stl10'         : [0.44671062, 0.43980984, 0.40664645],
+	'lsun'          : [0.485, 0.456, 0.406], # copied from imagenet
+	'cub200'        : [0.48310599, 0.49175689, 0.42481980],
+	'dog120'        : [0.47809049, 0.44543245, 0.38800871],
+	'food101'       : [0.55774122, 0.44237375, 0.32717270],
+	'flower102'     : [0.51136059, 0.41595200, 0.34071067],
+	'tiny_imagenet' : [0.48234981, 0.44617516, 0.39390695],
+	'imagenet'      : [0.485, 0.456, 0.406]
 }
 
 stds = {
-	'mnist'     : [0.30810780],
-	'svhn'      : [0.19803012, 0.20101562, 0.19703614],
-	'cifar10'   : [0.24703223, 0.24348513, 0.26158784],
-	'cifar100'  : [0.26733429, 0.25643846, 0.27615047],
-	'stl10'     : [0.26034098, 0.25657727, 0.27126738],
-	'lsun'      : [0.229, 0.224, 0.225], # copied from imagenet
-	'cub200'    : [0.24212829, 0.23832704, 0.26648488],
-	'dog120'    : [0.27039561, 0.26119319, 0.25956830],
-	'food101'   : [0.25203338, 0.25410590, 0.24439417],
-	'flower102' : [0.26119214, 0.25937790, 0.30645496],
-	'imagenet'  : [0.229, 0.224, 0.225]
+	'mnist'         : [0.30810780],
+	'svhn'          : [0.19803012, 0.20101562, 0.19703614],
+	'cifar10'       : [0.24703223, 0.24348513, 0.26158784],
+	'cifar100'      : [0.26733429, 0.25643846, 0.27615047],
+	'stl10'         : [0.26034098, 0.25657727, 0.27126738],
+	'lsun'          : [0.229, 0.224, 0.225], # copied from imagenet
+	'cub200'        : [0.22814971, 0.22405523, 0.25914747],
+	'dog120'        : [0.25985241, 0.25299320, 0.25506458],
+	'food101'       : [0.25912008, 0.26311737, 0.26589110],
+	'flower102'     : [0.29568306, 0.24932568, 0.28895128],
+	'tiny_imagenet' : [0.26266909, 0.25351605, 0.26663900],
+	'imagenet'      : [0.229, 0.224, 0.225]
 }
 
-dataset_list = ['mnist', 'svhn', 'cifar10', 'cifar100', 'stl10', 'lsun', 'cub200', 'dog120', 'food101', 'flower102', 'imagenet']
+dataset_list = ['mnist', 'svhn', 'cifar10', 'cifar100', 'stl10', 'lsun', 'cub200', 'dog120', 'food101', 'flower102', 'tiny_imagenet', 'imagenet']
 
 
 def check_dataset(name):
@@ -88,7 +91,14 @@ def get_dataset_stats(name, device='cpu'):
 
 # https://discuss.pytorch.org/t/computing-the-mean-and-std-of-dataset/34949
 def calculate_stats(path):
-	dataset = ImageFolder(path, transform=transforms.Compose([transforms.CenterCrop(224), transforms.ToTensor()]))
+	dataset = ImageFolder(
+		path, 
+		transform=transforms.Compose([
+			transforms.Resize(256),
+			transforms.CenterCrop(224), 
+			transforms.ToTensor()
+		])
+	)
 	loader = torch.utils.data.DataLoader(dataset, batch_size=100)
 
 	mean = 0.0
@@ -207,7 +217,7 @@ def get_dataset(name, train, input_size=224, normalize=True, augment=True, num_s
 		dataset = torchvision.datasets.CIFAR100(root, train=train, download=True, transform=transform)
 	elif name == 'stl10':
 		dataset = torchvision.datasets.STL10(root, split='train' if train else 'test', download=True, transform=transform)		
-	elif name in ['lsun', 'cub200', 'dog102', 'food101', 'imagenet']:
+	elif name in ['lsun', 'cub200', 'dog120', 'food101', 'flower102', 'tiny_imagenet', 'imagenet']:
 		root = os.path.join(root, 'train' if train else 'val')
 		dataset = ImageFolder(root, transform=transform)
 
@@ -320,21 +330,12 @@ def download_dataset(name):
 	elif name == 'cub200':
 		os.makedirs(root, exist_ok=True)
 
-		if not os.path.exists(os.path.join(root, 'images.tgz')):
-			url = 'http://www.vision.caltech.edu/visipedia-data/CUB-200/images.tgz'
-			urllib.request.urlretrieve(url, os.path.join(root, 'images.tgz'))
-
-		if not os.path.exists(os.path.join(root, 'lists.tgz')):
-			url = 'http://www.vision.caltech.edu/visipedia-data/CUB-200/lists.tgz'
-			urllib.request.urlretrieve(url, os.path.join(root, 'lists.tgz'))
+		if not os.path.exists(os.path.join(root, 'CUB_200_2011.tgz')):
+			url = 'http://www.vision.caltech.edu/visipedia-data/CUB-200-2011/CUB_200_2011.tgz'
+			urllib.request.urlretrieve(url, os.path.join(root, 'CUB_200_2011.tgz'))
 
 		if not os.path.exists(os.path.join(root, 'images')):
-			with tarfile.open(os.path.join(root, 'images.tgz'), 'r:gz') as tf:
-				tf.extractall(os.path.join(root))
-
-		if not os.path.exists(os.path.join(root, 'lists')):
-			with tarfile.open(os.path.join(root, 'lists.tgz'), 'r:gz') as tf:
-				tf.extractall(os.path.join(root))
+			os.system('tar -xzf {} --strip-components 1 -C {}'.format(os.path.join(root, 'CUB_200_2011.tgz'), root))
 
 		# make directories
 		os.makedirs(os.path.join(root, 'train'), exist_ok=True)
@@ -344,19 +345,21 @@ def download_dataset(name):
 				os.makedirs(os.path.join(root, 'train', d), exist_ok=True)
 				os.makedirs(os.path.join(root, 'val', d), exist_ok=True)
 
-		with open(os.path.join(root, 'lists/train.txt'), 'r') as f:
+		images = []
+		with open(os.path.join(root, 'images.txt'), 'r') as f:
 			lines = f.readlines()
 			for line in lines:
-				line = line.strip() # remove new line
-				if os.path.exists(os.path.join(root, 'images', line)):
-					shutil.move(os.path.join(root, 'images', line), os.path.join(root, 'train', line))
+				images.append(line.strip().split()[1])
 
-		with open(os.path.join(root, 'lists/test.txt'), 'r') as f:
+		with open(os.path.join(root, 'train_test_split.txt'), 'r') as f:
 			lines = f.readlines()
-			for line in lines:
-				line = line.strip() # remove new line
-				if os.path.exists(os.path.join(root, 'images', line)):
-					shutil.move(os.path.join(root, 'images', line), os.path.join(root, 'val', line))
+			for i, line in enumerate(lines):
+				label = line.strip().split()[1]
+				print(images[i], label)
+				if label == '0':
+					shutil.move(os.path.join(root, 'images', images[i]), os.path.join(root, 'val', images[i]))
+				elif label == '1':
+					shutil.move(os.path.join(root, 'images', images[i]), os.path.join(root, 'train', images[i]))
 
 		shutil.rmtree(os.path.join(root, 'images'))
 
@@ -403,7 +406,7 @@ def download_dataset(name):
 		shutil.rmtree(os.path.join(root, 'images'))
 		del matdata
 
-	# === food101 === #
+	# === Food-101 === #
 	elif name == 'food101':
 		os.makedirs(root, exist_ok=True)
 
@@ -438,7 +441,7 @@ def download_dataset(name):
 
 		shutil.rmtree(os.path.join(root, 'images'))
 
-	# === flower102 === #
+	# === Flower-102 === #
 	elif name == 'flower102':
 		os.makedirs(root, exist_ok=True)
 
@@ -474,6 +477,45 @@ def download_dataset(name):
 			label = labels_mat['labels'][id - 1] - 1
 			shutil.move(os.path.join(root, 'jpg/image_{:05d}.jpg'.format(id)), os.path.join(root, 'val/{:03d}/image_{:05d}.jpg'.format(label, id)))
 
+	# === tiny ImageNet === #
+	elif name == 'tiny_imagenet':
+		os.makedirs(root, exist_ok=True)
+
+		if not os.path.exists(os.path.join(root, 'tiny-imagenet-200.zip')):
+			url = 'http://cs231n.stanford.edu/tiny-imagenet-200.zip'
+			urllib.request.urlretrieve(url, os.path.join(root, 'tiny-imagenet-200.zip'))
+
+		# rename root directories
+		if not os.path.exists(os.path.join(root, 'train')):
+			with zipfile.ZipFile(os.path.join(root, 'tiny-imagenet-200.zip')) as z:
+				z.extractall(root)
+			shutil.move(os.path.join(root, 'tiny-imagenet-200/train'), os.path.join(root, 'train'))
+			shutil.move(os.path.join(root, 'tiny-imagenet-200/val'), os.path.join(root, 'val'))
+			shutil.move(os.path.join(root, 'tiny-imagenet-200/test'), os.path.join(root, 'test'))
+
+		# move train images
+		for d in os.listdir(os.path.join(root, 'train')):
+			os.system('mv {} {}'.format(
+				os.path.join(root, 'train', d, 'images/*'),
+				os.path.join(root, 'train', d)
+			))
+			os.rmdir(os.path.join(root, 'train', d, 'images'))
+			os.remove(os.path.join(root, 'train', d, '{}_boxes.txt'.format(d)))
+
+		# move val images
+		with open(os.path.join(root, 'val/val_annotations.txt'), 'r') as f:
+			reader = csv.reader(f, delimiter='\t')
+			for line in reader:
+				os.makedirs(os.path.join(root, 'val', line[1]), exist_ok=True)
+				shutil.move(
+					os.path.join(root, 'val/images', line[0]),
+					os.path.join(root, 'val', line[1])
+				)
+		
+		os.rmdir(os.path.join(root, 'val/images'))
+		os.remove(os.path.join(root, 'val/val_annotations.txt'))
+
+		shutil.rmtree(os.path.join(root, 'tiny-imagenet-200'))
 
 
 class CustomTensorDataset(Dataset):
